398d2cf69af393ac3a72ce02e29049ad3b44f75f
Christopher Garcia <DotChris1111@yahoo.com>
2019-07-23 02:10:56 -0500

    Websockets are great...

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-const express = require('express'),
-      app     = express(),
-      port    = process.env.PORT || 8095,
-      routes  = require('./routes/routes');
+const express       = require('express'),
+      app           = express(),
+      expressWs     = require('express-ws')(app),
+      bodyParser    = require('body-parser'),
+      port          = process.env.PORT || 8095,
+      routes        = require('./routes/routes')
 
-app.use( '/', routes );
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use( '/', routes )
 
 app.use( ( req, res ) => {
-    res.write('<h2>404 - Not Found</h2>');
-    res.status(404).end();
+    res.write('<h2>404 - Not Found</h2>')
+    res.status(404).end()
 });
 
-app.listen( port, () => console.log('Server running on port ' + port) );
+app.listen( port, () => console.log('Server running on port ' + port) )
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,30 @@
-const express = require('express'),
-      router = express.Router();
+const express       = require('express'),
+      router        = express.Router()
 
 router.get('/', (req, res) => {
-    res.write("<h2>Status: 200 OK</h2>");
-    res.status(200).end();
-});
+    console.log("Ping")
+    res.write("<h2>Status: 200 OK</h2>")
+    res.status(200).end()
+})
+
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    console.log("Pong")
+    res.status(200).end()
+})
+
+router.ws('/test', (ws, req) => {
+    ws.send('Connection established!')
+
+    ws.onmessage = (msg) => {
+        console.log( msg.data )
+        ws.send("I see you said '" + msg.data + "'" )
+    }
+
+    ws.onclose = () => {
+        console.log('WebSocket was closed')
+    }
+})
 
 module.exports = router;
