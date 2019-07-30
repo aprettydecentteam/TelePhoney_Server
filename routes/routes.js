0000f24e0149b0e2697f140f7f7149bcd2843930
ChrisBushman <christopherwbushman@gmail.com>
2019-07-30 14:15:23 -0500

    removed stringify

--- routes/routes.js
+++ routes/routes.js
@@ -164,7 +164,7 @@ router.ws( '/connectdemo', async ( ws, req ) => {
     clients.push(client);
     connectMessage.id = client.id;
     connectMessage.msgEvent = "connected";
-    ws.send(JSON.stringify(connectMessage));
+    ws.send(connectMessage);
 });
 
 router.post( '/roledemo', ( req, res ) => {
