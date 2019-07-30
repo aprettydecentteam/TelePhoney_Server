c2d8788a08e91393432cb41b512e291dbc576734
Chris Bushman <christopherwbushman@gmail.com>
2019-07-29 21:55:35 -0500

    Refactored connect route

--- routes/routes.js
+++ routes/routes.js
@@ -33,30 +33,43 @@ router.ws( '/test', ( ws, req ) => {
 
 router.post( '/newPlayer', async ( req, res ) => {
     let playerName = req.body.playerName;
+    console.log(playerName);
     try {
         res.json( await userManager.lookupUser(playerName) );
     } catch ( e ) {
+        console.log("Error is " + e);
         res.status(500).json( {error: e} );
     }
 });
 
-router.ws( '/joinSession', async ( ws, req ) => {
+router.ws( '/connect', async ( ws, req ) => {
+    console.log("joinSession was connected to");
     ws.send("Finding a session to join...");
 
     ws.onclose = () => {
       sessionManager.closeSession( ws.getWSS().clients, req.playerId );
     };
 
-    try {
+    ws.onmessage = (msg) => {
         //we want the playerId here because this was made by the server
         //this acts as the "authentication" in our prototype
-        let session = await sessionManager.joinSession( ws, req.playerId );
+        let request = JSON.parse(msg.data);
+        console.log("Here is the serialized message: " + msg.data);
+        console.log("Here is the JSON object: " + JSON.stringify(request));
+        if (request.reqEvent = "joinSession")
+        {
+            try {
+                console.log(request.playerId);
+                let playerID = request.playerId;
+                let session = sessionManager.joinSession( ws, playerID );
 
-        ws.send("Connected to session");
-        ws.send( session )
-    } catch ( e ) {
-        ws.send("Sorry, there was an error joining the session.");
-        ws.terminate();
+                ws.send("Connected to session");
+                ws.send( session )
+            } catch ( e ) {
+                ws.send("Sorry, there was an error joining the session.");
+                ws.terminate();
+            }
+        }
     }
 });
 module.exports = router;
