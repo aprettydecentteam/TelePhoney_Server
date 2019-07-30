c01b065f7997c4cdce3d65edd6a5d54607ebdf44
ChrisBushman <christopherwbushman@gmail.com>
2019-07-30 14:23:29 -0500

    await ws call

--- routes/routes.js
+++ routes/routes.js
@@ -164,7 +164,8 @@ router.ws( '/connectdemo', async ( ws, req ) => {
     clients.push(client);
     connectMessage.id = client.id;
     connectMessage.msgEvent = "connected";
-    ws.send(JSON.stringify(connectMessage));
+    await ws.send(JSON.stringify(connectMessage));
+    console.log("sent: ");
 });
 
 router.post( '/roledemo', ( req, res ) => {
